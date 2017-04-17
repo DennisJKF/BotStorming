24529caaf046666b3bb875fe0d7faae1da9541bd
Dennis Jiang <dennis.jiang@LMXMN076.local>
2017-04-17 16:13:13 +0800

    Remove credential.

--- run.sh
+++ run.sh
@@ -2,9 +2,9 @@ npm install wordnet-db
 npm install speakeasy-nlp
 npm install natural
 HUBOT_GLIP_SERVER=https://platform.ringcentral.com \
-HUBOT_GLIP_APP_KEY=MkCdlSVqQ06H6i7KYcv9bg \
-HUBOT_GLIP_APP_SECRET=5_tFBXBQTLWaVcPF61LUGgngBfc8KGQCaZ0_UTw80vsw \
-HUBOT_GLIP_USERNAME=16507419631 \
-HUBOT_GLIP_EXTENSION=108 \
-HUBOT_GLIP_PASSWORD=Test!123 \
+HUBOT_GLIP_APP_KEY= \
+HUBOT_GLIP_APP_SECRET= \
+HUBOT_GLIP_USERNAME= \
+HUBOT_GLIP_EXTENSION= \
+HUBOT_GLIP_PASSWORD= \
 ./bin/hubot -a glip -n bs
